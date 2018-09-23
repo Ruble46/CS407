4581ae75a8dea30b3e730fc5dbe7eea79176ca39
Kyle812 <kleaf.gbit@gmail.com>
2018-09-23 14:05:53 -0400

    Adding files for api such as AppUser models, AppRole models, ApplicationDbContext, and sample AccountController. Solution successfully builds but can't publish to Azure currently

---
+++ API/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Game2gether.API.Models;
+
+namespace Game2gether.API
+{
+    public class ApplicationDbContext : IdentityDbContext <AppUser, AppRole, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ API/Models/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace Game2gether.API.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string testProp { get; set; }
+    }
+}
