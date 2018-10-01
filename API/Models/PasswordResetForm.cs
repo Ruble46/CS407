1b9148ef58d4312fb222f5e1d89de81d85f12ad1
Kyle812 <kleaf.gbit@gmail.com>
2018-10-01 18:15:33 -0400

    added password reset functionality

---
+++ API/Models/PasswordResetForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Game2gether.API.Models
+{
+    public class PasswordResetForm
+    {
+        public string token { get; set; }
+        public string email { get; set; }
+        public string newPassword { get; set; }
+        public string oldPassword { get; set; }
+    }
+}
