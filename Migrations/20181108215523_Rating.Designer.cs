ff45cac756249dfa1cbf2d31e8ee2b7c0913f72f
TebanSierra <esteban.sierram@gmail.com>
2018-11-08 17:10:41 -0500

    Added Rating Schema

--- Migrations/20181108215523_Rating.Designer.cs
+++ Migrations/20181108215523_Rating.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Game2gether.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181108215523_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
