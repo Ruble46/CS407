36d44387a9bad4acf1f0440a99e050868804bda0
Esteban Sierra Múnera <esteban.sierram@gmail.com>
2018-12-03 17:12:54 -0500

    Rebase conflicts

--- API/Models/AppUser.cs
+++ API/Models/AppUser.cs
@@ -16,8 +16,8 @@ namespace Game2gether.API.Models
         public string friends { get; set; }
         public string friendRequests { get; set; }
         public string steamId { get; set; }
-        public string steamAvatar { get; set; }
         public string steamName { get; set; }
+        public string steamAvatar { get; set; } 
         public string games { get; set; }
     }
 }
